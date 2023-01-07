c894b0768dd225c5e1e0296c0c36f2aeacf93e64
unknown <gbemilesanmi@gmail.com>
2023-01-08 00:52:45 +0100

    Added some comments to hex.js in colorflipper

--- colorflipper/hex.js
+++ colorflipper/hex.js
@@ -8,6 +8,9 @@ button.addEventListener('click' , function (){
     color.textContent = random 
 })
 
+//NOTE: get a random hex color, hex colors consist o g digits
+//WARN: chamge name 
+
 const getrandomnumber = () => {
     let hex = '#'
     for (let i = 0 ; i < 6 ; i++ ){
